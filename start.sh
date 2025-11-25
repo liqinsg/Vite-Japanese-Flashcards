a4f25295c438379e4e3976332f456659c290b35d
liqinsg <liqin@hotmail.sg>
2025-11-25 12:19:02 +0800

    some changes

--- start.sh
+++ start.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/bash
+
+# Default commit message
+DEFAULT_MSG="some changes"
+COMMIT_MSG=""
+
+# Check for the -m option
+while getopts "m:" opt; do
+  case ${opt} in
+    m )
+      COMMIT_MSG="$OPTARG"
+      ;;
+    \? )
+      echo "Usage: cmd [-m commit_message]"
+      exit 1
+      ;;
+  esac
+done
+
+# If COMMIT_MSG is empty, use the default message
+COMMIT_MSG="${COMMIT_MSG:-$DEFAULT_MSG}"
+
+# Check if there are changes to commit
+if [[ -z $(git status --porcelain) ]]; then
+  echo "Nothing to commit, working tree clean"
+  exit 0
+fi
+
+# Git commands
 git add .
-git commit -m "some changes"
-git push origin main
-npm run deploy
+git commit -m "$COMMIT_MSG"
+
+# Determine current branch and push
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$BRANCH_NAME"
